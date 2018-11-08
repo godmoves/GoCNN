2879feb65acfebb6b651ce28d065ea8d1f300257
ManKit <514158072@qq.com>
2018-11-08 21:43:02 +0800

    clean up data preprocessing

--- munge/bit_writer.py
+++ munge/bit_writer.py
@@ -1,41 +1,42 @@
 
 import numpy as np
 
+
 def write_sequence(sequence, datafile):
-	bit_pos = 1
-	this_byte = 0
-	num_bytes = 0
-	for bit in sequence:
-		if bit == 1:
-			this_byte = this_byte | bit_pos
-		bit_pos *= 2
-		if bit_pos >= 256:
-			bit_pos = 1
-			datafile.write(chr(this_byte))
-			num_bytes += 1
-			this_byte = 0
-	if bit_pos > 1:
-		datafile.write(chr(this_byte))
-		num_bytes += 1
-	return num_bytes
+    bit_pos = 1
+    this_byte = 0
+    num_bytes = 0
+    for bit in sequence:
+        if bit == 1:
+            this_byte = this_byte | bit_pos
+        bit_pos *= 2
+        if bit_pos >= 256:
+            bit_pos = 1
+            datafile.write(chr(this_byte))
+            num_bytes += 1
+            this_byte = 0
+    if bit_pos > 1:
+        datafile.write(chr(this_byte))
+        num_bytes += 1
+    return num_bytes
+
 
 def read_sequence(datafile, num_bits):
-	bits_read = 0
-	bit_pos = 256
-	this_byte = 0
-	sequence = np.zeros(num_bits)
-	while(bits_read < num_bits):
-		if bit_pos >= 256:
-			bit_pos = 1
-			this_byte = ord(datafile.read(1))
-			#print this_byte
-			#if not this_byte:
-			#	raise EOFError("At the end of the file")
-		if this_byte&bit_pos != 0:
-			sequence[bits_read] = 1.0
-		else:
-			sequence[bits_read] = 0.0
-		bit_pos *=2
-		bits_read +=1
-	return sequence
-
+    bits_read = 0
+    bit_pos = 256
+    this_byte = 0
+    sequence = np.zeros(num_bits)
+    while(bits_read < num_bits):
+        if bit_pos >= 256:
+            bit_pos = 1
+            this_byte = ord(datafile.read(1))
+            # print this_byte
+            # if not this_byte:
+            #     raise EOFError("At the end of the file")
+        if this_byte & bit_pos != 0:
+            sequence[bits_read] = 1.0
+        else:
+            sequence[bits_read] = 0.0
+        bit_pos *= 2
+        bits_read += 1
+    return sequence
--- thirdparty/Bag2d.py
+++ thirdparty/Bag2d.py
@@ -1,27 +1,29 @@
 # Copyright Hugh Perkins 2015 hughperkins at gmail
 #
-# This Source Code Form is subject to the terms of the Mozilla Public License, 
-# v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 
 # doubly-indexed collection of locations on a board
 # - given a location, can determine if it exists in the 'bag' of lcoations, in O(1)
 # - can iterate over the locations, in time O(1) per locations
 # - can remove a location, in O(1)
+
+
 class Bag2d(object):
     def __init__(self, boardSize):
         self.boardSize = boardSize
         self.pieces = []
         self.board = {}
 
-    def insert( self, combo ):
-        ( row, col ) = combo
+    def insert(self, combo):
+        (row, col) = combo
         if self.board.has_key(combo):
             return
-        self.pieces.append( combo )
-        self.board[combo] = len( self.pieces ) - 1
+        self.pieces.append(combo)
+        self.board[combo] = len(self.pieces) - 1
 
-    def erase( self, combo ):
+    def erase(self, combo):
         if not self.board.has_key(combo):
             return
         i1d = self.board[combo]
@@ -35,24 +37,23 @@ class Bag2d(object):
         self.board[movedcombo] = i1d
         del self.board[combo]
 
-    def exists( self, combo ):
+    def exists(self, combo):
         return self.board.has_key(combo)
 
     def size(self):
         return len(self.pieces)
 
-    def __getitem__( self, i1d ):
+    def __getitem__(self, i1d):
         return self.pieces[i1d]
 
     def __str__(self):
         result = 'Bag2d\n'
-        for row in range(self.boardSize - 1, -1, -1 ):
+        for row in range(self.boardSize - 1, -1, -1):
             thisline = ""
             for col in range(0, self.boardSize):
-                if self.exists( (row, col) ):
+                if self.exists((row, col)):
                     thisline = thisline + "*"
                 else:
                     thisline = thisline + "."
             result = result + thisline + "\n"
         return result
-
