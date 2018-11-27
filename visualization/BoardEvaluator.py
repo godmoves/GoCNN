fix unwanted rotation

visualization/BoardEvaluator.py
@@ -2,8 +2,6 @@
 
 # BoardEvaluator.py
 import tensorflow as tf
-import sys
-# sys.path.append("../board_evaluation")
 from board_evaluation import model
 import numpy as np
 
@@ -51,8 +49,8 @@ class BoardEvaluator:
         return np.reshape(y_pred, [19, 19])
 
     # board - GoBoard object
-    # returns [19,19] matrix of floats, each float in [0,1] indicating probability black owns the territory
-    # at the end of the game
+    # returns [19,19] matrix of floats, each float in [0,1] indicating
+    # probability black owns the territory at the end of the game
     def evaluate_board(self, goBoard, color_to_move):
         feature_cube = _board_to_feature_cube(goBoard, color_to_move)
         predicted_ownership = self.predict_single_sample(feature_cube)

visualization/GTP.py
@@ -17,20 +17,17 @@ import numpy as np
 import os
 from GoDriver import GoDriver
 
-MODEL_PATH = "./data/working/test.ckpt"
-
-# everytime we reset the board we will load a random game from this directory to view
-SGF_DIRECTORY = "./visualization/checkpoint"
 
 N = 9  # size of the board
 letter_coords = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
+
 
 # sgf_dir - string, directory containing sgf files
 # returns list of strings
 def get_sgf_filelist(sgf_dir):
     print("Looking for games in %s" % sgf_dir, file=sys.stderr)
     sgf_files = []
-    for subdir, dirs, files in os.walk(SGF_DIRECTORY):
+    for subdir, dirs, files in os.walk(sgf_dir):
         for file in files:
             filepath = os.path.join(subdir, file)
             if filepath.endswith(".sgf"):
@@ -41,31 +38,34 @@ def get_sgf_filelist(sgf_dir):
 
 # go from matrix index to board position string e.g. 0,2 -> A3
 def coord_to_str(row, col):
-    return letter_coords[row] + str(col + 1)
+    # return letter_coords[row] + str(col + 1)
+    return letter_coords[col] + str(row + 1)
 
 
 # ownership_matrix - [N,N] matrix of floats output from the CNN model
-# Formats a valid response string that can be fed into gogui as response to the 'predict_ownership' command
+# Formats a valid response string that can be fed into gogui as response to the
+# 'predict_ownership' command
 def influence_str(ownership_matrix):
     print("Score without komi: %.2f" % np.sum(2 * ownership_matrix - 1), file=sys.stderr)
     rtn_str = "INFLUENCE "
     for i in range(len(ownership_matrix)):
         for j in range(len(ownership_matrix)):
-            rtn_str += "%s %.1lf " % (coord_to_str(i, j), 2 * (ownership_matrix[i][j] - .5))  # convert to [-1,1] scale
+            # convert to [-1,1] scale
+            rtn_str += "%s %.1lf " % (coord_to_str(i, j), 2 * (ownership_matrix[i][j] - .5))
             # rtn_str+= " %.1lf\n" %(ownership_matrix[i][j])
     return rtn_str
 
 
-def gtp_io():
+def gtp_io(sgf_dir, model_path):
     """ Main loop which communicates to gogui via GTP"""
     known_commands = ['boardsize', 'clear_board', 'komi', 'play', 'genmove',
                       'final_score', 'quit', 'name', 'version', 'known_command',
                       'list_commands', 'protocol_version', 'gogui-analyze_commands']
     analyze_commands = ["gfx/Predict Final Ownership/predict_ownership",
                         "none/Load New SGF/loadsgf"]
-    sgf_files = get_sgf_filelist(SGF_DIRECTORY)
+    sgf_files = get_sgf_filelist(sgf_dir)
     sgf_file = random.choice(sgf_files)
-    driver = GoDriver(sgf_file, MODEL_PATH)
+    driver = GoDriver(sgf_file, model_path)
 
     print("starting main.py: loading %s" % sgf_file, file=sys.stderr)
     output_file = open("output.txt", "wb")

visualization/GoDriver.py
@@ -2,13 +2,12 @@
 
 # GoDriver.py
 from __future__ import print_function
-import numpy as np
 import sys
 
-# TODO: fix this relative path, it will cause an import error if gogui is not started from the visualization directory
-# sys.path.append("../thirdparty")
-from thirdparty import GoBoard  # this is in the thirdparty directory
+import numpy as np
 import gomill.sgf
+
+from thirdparty import GoBoard  # this is in the thirdparty directory
 from visualization.BoardEvaluator import BoardEvaluator
 
 
@@ -20,14 +19,15 @@ def _swap_color(color):
     raise ValueError("color needs to be w or b")
 
 
-'''
-GoDriver has a pointer to a gomill.Sgf_game object which contains the game tree defined by an sgf file.
-The current position in the file in maintained by the Sgf_game.main_sequence_iter() iterator.
-It also contains a BoardEvaluator object which will load the tensorflow model and be able to make predictions
-based on the current board. '''
-
-
 class GoDriver:
+    '''
+        GoDriver has a pointer to a gomill.Sgf_game object which contains the
+        game tree defined by an sgf file. The current position in the file in
+        maintained by the Sgf_game.main_sequence_iter() iterator. It also
+        contains a BoardEvaluator object which will load the tensorflow model
+        and be able to make predictions based on the current board.
+    '''
+
     def __init__(self, sgf_filepath, tf_ckpt_path, BOARD_SIZE=19):
         self.board_evaluator = BoardEvaluator(tf_ckpt_path)
         self.BOARD_SIZE = BOARD_SIZE
@@ -65,8 +65,8 @@ class GoDriver:
         self.board.applyMove(color, (row, col))
         return row, col
 
-    # returns [19,19] matrix of floats indicating the probability black will own the territory at the end
-    # of the game
+    # returns [19,19] matrix of floats indicating the probability black will own
+    # the territory at the end of the game
     def evaluate_current_board(self):
         if self.board is None:
             return np.zeros((19, 19))