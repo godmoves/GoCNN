643a02f75bba237fa024af6630d15ae990015df5
godmoves <514158072@qq.com>
2018-12-22 17:13:50 +0800

    add script to build pachi, using two spaces in shell

--- thirdparty/build_gnugo.sh
+++ thirdparty/build_gnugo.sh
@@ -4,11 +4,11 @@ GNUGO="gnugo-3.8/interface/gnugo"
 cd $(dirname $0)
 
 if [ -e $GNUGO ]; then
-    echo "gnugo already exists"
+  echo "gnugo already exists"
 else
-    wget http://ftp.gnu.org/gnu/gnugo/gnugo-3.8.tar.gz
-    tar -xzf gnugo-3.8.tar.gz
-    cd gnugo-3.8
-    ./configure
-    make
+  wget http://ftp.gnu.org/gnu/gnugo/gnugo-3.8.tar.gz
+  tar -xzf gnugo-3.8.tar.gz
+  cd gnugo-3.8
+  ./configure
+  make
 fi
